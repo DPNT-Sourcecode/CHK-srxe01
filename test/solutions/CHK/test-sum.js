09be9189251a0ec8253067ca2af383cac3fac0ba
sbx_user1051 <sbx_user1051@169.254.231.181>
2020-12-16 19:02:03 +0000

    Wed Dec 16 19:02:03 UTC 2020

--- test/solutions/CHK/test-sum.js
+++ test/solutions/CHK/test-sum.js
@@ -2,10 +2,12 @@ var mocha = require('mocha')
 var describe = mocha.describe
 var it = mocha.it
 var assert = require('assert');
-const sum = require('../../../lib/solutions/SUM/sum');
+const checkout = require('../../../lib/solutions/CHK/checkout');
 
-describe('SUM challenge: adding two numbers', function() {
-	it('should return 3, which is the sum of 1 and 2', function() {
-	    assert.equal(sum(1, 2), 3);
-	});
+describe('CHK challenge: single item cart', function() {
+	it('should return the correct price for single items', () => {
+		assert.equal(checkout('A'), 50)
+		assert.equal(checkout('B'), 30)
+		assert.equal(checkout('C'), 20)
+	})
 });
