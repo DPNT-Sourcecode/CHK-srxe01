82719bea0ab51d36aff0456aede1b0b0282fa74a
sbx_user1051 <sbx_user1051@169.254.231.181>
2020-12-16 19:57:57 +0000

    Wed Dec 16 19:57:57 UTC 2020

--- test/solutions/CHK/test-chk.js
+++ test/solutions/CHK/test-chk.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 const checkout = require('../../../lib/solutions/CHK/checkout');
 
 describe('CHK challenge: single item cart', function() {
-	it('should return the correct price for single items', () => {
+	/* it('should return the correct price for single items', () => {
 		assert.equal(checkout('A'), 50)
 		assert.equal(checkout('B'), 30)
 		assert.equal(checkout('C'), 20)
@@ -15,10 +15,10 @@ describe('CHK challenge: single item cart', function() {
 		assert.equal(checkout('a'), -1)
 		assert.equal(checkout('-'), -1)
 		assert.equal(checkout('ABCa'), -1)
-	})
+	}) */
 
 	it('should handle free offers correctly', () => {
-		assert.equal(checkout('EEB'), 80)
+		/* assert.equal(checkout('EEB'), 80)
 		assert.equal(checkout('EB'), 70)
 		assert.equal(checkout('AAAAEEB'), 130 + 50 + 80)
 		assert.equal(checkout('AAAAAEEB'), 200 + 80)
@@ -26,11 +26,16 @@ describe('CHK challenge: single item cart', function() {
 
 		assert.equal(checkout('AAAAAAAAAA'), 400)
 		assert.equal(checkout('EEEEBB'), 160)
-		assert.equal(checkout('BEBEEE'), 160)
+		assert.equal(checkout('BEBEEE'), 160) */
+
+		assert.equal(checkout('EE'), 80)
+
+		assert.equal(checkout('ABCDEABCDE'), 280)
+		assert.equal(checkout('CCADDEEBBA'), 280)
 	})
 });
 
 /*
-- {"method":"checkout","params":["AAAAAAAAAA"],"id":"CHK_R2_022"}, expected: 400, got: 430
-- {"method":"checkout","params":["EEEEBB"],"id":"CHK_R2_026"}, expected: 160, got: 175
-- {"method":"checkout","params":["BEBEEE"],"id":"CHK_R2_027"}, expected: 160, got: 175 */
+ - {"method":"checkout","params":["EE"],"id":"CHK_R2_023"}, expected: 80, got: 50
+ - {"method":"checkout","params":["ABCDEABCDE"],"id":"CHK_R2_038"}, expected: 280, got: 265
+ - {"method":"checkout","params":["CCADDEEBBA"],"id":"CHK_R2_039"}, expected: 280, got: 265 */
